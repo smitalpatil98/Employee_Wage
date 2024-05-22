d96bd61cf6bae9561b9fb11cf685d8d092be4402
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-22 12:36:03 +0530

    UC6_total_working_hours_reached

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -6,14 +6,16 @@ public class EmpWageBuilderUC1
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 
 	public static void main(String[] args) 
 	{	
 		//Variables
-		int empHrs = 0,empWage = 0,totalEmpWage= 0;
+		int empHrs = 0,totalEmpHrs = 0,totalWorkingDays = 0;
 		//computation
-		for(int day = 0;day<NUM_OF_WORKING_DAYS;day++) 
-		{
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && 
+				totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
 			case  IS_PART_TIME:
@@ -26,9 +28,11 @@ public class EmpWageBuilderUC1
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: "+ empWage);
-		}	
-	}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: "+ totalWorkingDays + "Emp Hr: " +empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("tOTAL Emp Wage: "+ totalEmpWage);
+	}	
 }
+
