97cdacac8bac30fc424f1719421c59a20a77e8cd
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-22 11:17:40 +0530

    UC_2

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -10,6 +10,10 @@ public class EmpWageBuilderUC1
 	{
 		//Constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
 		//computation
 		double empCheck = Math.floor(Math.random()*10)%2;
 		//Math.floor() is used primarily to round a floating-point number down to the nearest integer value. 
@@ -17,9 +21,11 @@ public class EmpWageBuilderUC1
 		//This function is commonly used when you need to generate random numbers for various purposes, such as simulations, games, or cryptographic applications.
 		System.out.println(empCheck);
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+ empWage);
 	}	
 }
 
